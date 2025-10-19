f342dbc6e506c16f3dabbed8a44b4590e3e94250
ffafnir <ffafnir@192.168.1.25>
2025-10-19 15:52:50 +0700

    Add TakeDamage method and Mana Fields

--- Entity.cs
+++ Entity.cs
@@ -3,24 +3,20 @@ namespace Consolas;
 class Entity(string name)
 {
   private int _attack;
-  private int _health = 100;
+  private int _mana;
+  private int _health;
   public string EntityName { get; set; } = name;
-  public int EntityHealth { get => _health; set
-  {
-    if (_health < 0)
-    {
-       value = 0; 
-    }
-    else if (_health > 100)
-    {
-       value = 100; 
-    }
-  } 
-  }
+  public int EntityHealth { get => _health; set => _health = value;}
+  public int EntityMana{ get => _mana; set => _mana = value; }
   public virtual string DisplayName() => EntityName;
   public virtual int Attack(int attack){
     _attack = attack;
     return _attack;
+  }
+  public int TakeDamage()
+  {
+    int takeDamage = _health - Attack(_attack);
+    return takeDamage;
   }
 }
 
@@ -28,11 +24,14 @@ class Player(string name) : Entity(name)
 {
   public string PlayerName { get; set; } = name;
   public override string DisplayName() => PlayerName;
+  public override int Attack(int attack) => attack;
 }
 
 class Enemy(string name) : Entity(name)
 {
-
+  public string EnemyName {get;set;} = name;
+  public override string DisplayName() => EnemyName;
+  public override int Attack(int attack) => attack;
 }
 
 
---
+++ Game.cs
@@ -0,0 +1,11 @@
+namespace Consolas;
+
+class UI
+{
+  
+}
+
+class Game
+{
+    
+}
